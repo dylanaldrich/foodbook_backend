a3ba25eb3654aba058d95e4de987974df3ba418a
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-27 16:31:05 -0400

    adds user model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/, // Email must match typical email format: xxx@yyy.zzz
+        },
+        password: {
+            type: String,
+            required: true,
+            match: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.[\W]).{8,}$/, // Password must have at least 8 characters, lowercase, Uppercase, number, non-alphanumeric symbol
+        }
+    },
+    {timestamps: true}
+);
+
+userSchema.set("toJSON", {
+    transform: (doc, ret, opt) => {
+        delete ret["password"];
+        return ret;
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
