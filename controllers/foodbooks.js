4f713b7b4ced40320ca6ae7a87c88b06e8f5e24b
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-28 21:59:01 -0400

    refactors foodbooks delete route

--- controllers/foodbooks.js
+++ controllers/foodbooks.js
@@ -75,29 +75,38 @@ router.put('/:foodbookId', async (req, res) => {
 // foodbook delete
 router.delete('/:foodbookId', async (req, res) => {
     try {
-        // find foodbook to be deleted, populate its recipes
+        // find foodbook to be deleted; populate its recipes and user
         const deletedFoodbook = await db.Foodbook.findById(req.params.foodbookId)
-            .populate({path: 'recipes'}).exec();
-
+        .populate('Recipes')
+        .populate('User')
+        .exec();
+        
         // extra failsafe to handle if foodbook doesn't exist
         if(!deletedFoodbook) return res.status(200).json({message: "Sorry, that foodbook doesn't exist in our database. Please try again."}); 
-
+        
         // verify that the current user is the owner of the foodbook
-        if(foundFoodbook.user._id === req.userId){
-            // update/delete recipes stored the deleted foodbook
-            const recipesToRemove = deletedFoodbook.recipes;
-            for (recipe in recipesToRemove) {
-                // if a recipe is only saved to one foodbook, delete the recipe entirely
-                if (recipe.foodbooks.length <= 1) {
-                    recipe.deleteOne();
-                } else {
-                    // otherwise, just remove deletedFoodbook from that recipe's foodbooks array, therefore the recipe can remain saved in other foodbooks
-                    recipe.foodbooks.remove(deletedFoodbook);
-                    recipe.save();
+        if(deletedFoodbook.user._id === req.userId) {
+            // remove foodbook reference from user
+            const foodbookUser = deletedFoodbook.user;
+            await foodbookUser.foodbooks.remove(deletedFoodbook);
+            foodbookUser.save();
+
+            // remove foodbook reference(s) from associated recipes
+            if(deletedFoodbook.recipes.length) {
+                for(recipe in deletedFoodbook.recipes) {
+                    // if a recipe is only saved to one foodbook, delete the recipe entirely
+                    if (recipe.foodbooks.length = 1) {
+                        db.Recipe.findByIdAndDelete(recipe._id);
+                    } else {
+                        // TODO ask instructor if this is actually removing the foodbook association from the recipe document in the db (or if it needs to be done like above, by querying the db)
+                        // otherwise, just remove deletedFoodbook from that recipe's foodbooks array, therefore the recipe can remain saved in other foodbooks
+                        recipe.foodbooks.remove(deletedFoodbook);
+                        recipe.save();
+                    }
                 }
             }
 
-            // delete user from db
+            // delete foodbook from db
             deletedFoodbook.deleteOne();
 
             res.sendStatus(200).json({deletedFoodbook: deletedFoodbook});
